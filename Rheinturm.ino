5c300830daa2893d8d7be14c42b39a6a66eda3af
Thorsten Muelhoefer <muethos76@gmail.com>
2021-05-23 15:08:31 +0200

    Touch-Button (Modus+Long_Press)

--- Rheinturm.ino
+++ Rheinturm.ino
@@ -5,22 +5,39 @@
      See https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv for Timezone codes for your region
 */
 
+
 #include <SPIFFS.h>
 #include <WiFiSettings.h>
 #include <ArduinoOTA.h>
 #include <time.h>
 #include <Adafruit_NeoPixel.h>
 #define NUMPIXELS 60 // Anzahl LEDs
-#define PIN        12 // LED Pin
+#define LED_PIN        12 // LED Pin
+#define Touch_PIN 14 // Touchsensor
+#define Touch_Threshold 20
+//#define DEBUG 1
 struct ziffern {
   byte einer;
   byte zehner;
 } sekunden, minuten, stunden;
 
+struct color {
+  int r;
+  int g;
+  int b;
+};
 
+color uhr = {100, 80, 0};
+color aus = {0, 0, 0};
 
+int Modus = 1;
+int Touch_Value = 0;
+bool Touched = 0;
+unsigned long Old_Millis = 0;
+int Long_Touch = 0;
+unsigned long Press_Time;
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 const char* NTP_SERVER = "de.pool.ntp.org";
 const char* TZ_INFO    = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00";
@@ -38,7 +55,10 @@ void setup_ota() {
 }
 
 void setup() {
+
   Serial.begin(115200);
+
+
   SPIFFS.begin(true);  // Will format on the first run after failing to mount
 
   // Force WPA secured WiFi for the software access point.
@@ -71,136 +91,45 @@ void setup() {
 
   if (getNTPtime(10)) {  // wait up to 10sec to sync
   } else {
-    Serial.println("Time not set");
+    //Serial.println("Time not set");
     ESP.restart();
   }
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-
+  pixels.clear(); // Set all pixel colors to 'off'
 
 }
 
 void loop() {
   ArduinoOTA.handle();  // If you also want the OTA during regular execution
-
   // Your loop code here
-
-  //   pixels.clear(); // Set all pixel colors to 'off'
   getNTPtime(1);
-  //showTime(timeinfo);
+  display_clock();
 
+  Touch_Value = touchRead(Touch_PIN);
+  if (Touch_Value < Touch_Threshold && Touched == 0) {
+    Old_Millis = millis();
+    Touched = 1;
+  } else if (Touch_Value > Touch_Threshold && Touched == 1) {
+   Press_Time  = millis() - Old_Millis;
+    if (Press_Time > 200 && Press_Time < 1500) {
+      Modus++;
+      Touched = 0;
+    }else if (Press_Time > 1500) {
+      Long_Touch++;
+      Touched = 0;
+    }else{
+    Touched = 0;
+  }
 
-  //  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-  //    pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-  //    pixels.show();   // Send the updated pixel colors to the hardware.
-  //    delay(100); // Pause before next pass through loop
-  //}
-
-
-
-  // Uhr Berechnungen:
-  Serial.print(stunden.zehner);
+  }
+  Serial.print(Modus);
   Serial.print(" ");
-  Serial.print(stunden.einer);
-  Serial.print(" - ");
-  Serial.print(minuten.zehner);
+  Serial.print(Long_Touch);
   Serial.print(" ");
-  Serial.print(minuten.einer);
-  Serial.print(" - ");
-  Serial.print(sekunden.zehner);
+  Serial.print(Touched);
   Serial.print(" ");
-  Serial.print(sekunden.einer);
-  Serial.println(" ");
-
-  for (int i = 0; i <= 8; i++) {
-    if (i + 1 <= sekunden.einer) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-
-  for (int i = 9; i <= 13; i++) {
-    if (i - 8 <= sekunden.zehner) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-
-  for (int i = 14; i <= 22; i++) {
-    if (i - 13 <= minuten.einer) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-  for (int i = 23; i <= 27; i++) {
-    if (i - 22 <= minuten.zehner) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-  for (int i = 28; i <= 36; i++) {
-    if (i - 27 <= stunden.einer) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-  for (int i = 37; i <= 38; i++) {
-    if (i - 36 <= stunden.zehner) {
-      pixels.setPixelColor(i, pixels.Color(50, 0, 50));
-    } else {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    }
-  }
-  pixels.show();
-}
-
-
-
-
-bool getNTPtime(int sec) {
-  {
-    uint32_t start = millis();
-    do {
-      time(&now);
-      localtime_r(&now, &timeinfo);
-      //Serial.print(".");
-      delay(10);
-    } while (((millis() - start) <= (1000 * sec)) && (timeinfo.tm_year < (2016 - 1900)));
-    if (timeinfo.tm_year <= (2016 - 1900)) return false;  // the NTP call was not successful
-    //Serial.print("now ");  Serial.println(now);
-    char time_output[30];
-    //strftime(time_output, 30, "%a  %d-%m-%y %T", localtime(&now));
-    //Serial.println(time_output);
-    //Serial.println();
-    sekunden.einer = timeinfo.tm_sec % 10;
-    sekunden.zehner = (timeinfo.tm_sec / 10) % 10;
-    minuten.einer = timeinfo.tm_min % 10;
-    minuten.zehner = (timeinfo.tm_min / 10) % 10;
-    stunden.einer = timeinfo.tm_hour % 10;
-    stunden.zehner = (timeinfo.tm_hour / 10) % 10;
-  }
-  return true;
-}
-
-
-void showTime(tm localTime) {
-  Serial.print(localTime.tm_mday);
-  Serial.print('-');
-  Serial.print(localTime.tm_mon + 1);
-  Serial.print('-');
-  Serial.print(localTime.tm_year - 100);
-  Serial.print('_');
-  Serial.print(localTime.tm_hour);
-  Serial.print(':');
-  Serial.print(localTime.tm_min);
-  Serial.print(':');
-  Serial.print(localTime.tm_sec);
-  Serial.print(" Day of Week ");
-  if (localTime.tm_wday == 0)   Serial.println(7);
-  else Serial.println(localTime.tm_wday);
+  Serial.print(Press_Time);
+  Serial.print(" ");
+  Serial.println(Touch_Value);
 }
---
+++ uhr.ino
@@ -0,0 +1,63 @@
+
+
+bool getNTPtime(int sec) {
+  {
+    uint32_t start = millis();
+    do {
+      time(&now);
+      localtime_r(&now, &timeinfo);
+      //Serial.print(".");
+      delay(10);
+    } while (((millis() - start) <= (1000 * sec)) && (timeinfo.tm_year < (2016 - 1900)));
+    if (timeinfo.tm_year <= (2016 - 1900)) return false;  // the NTP call was not successful
+    //Serial.print("now ");  Serial.println(now);
+    char time_output[30];
+    //strftime(time_output, 30, "%a  %d-%m-%y %T", localtime(&now));
+    //Serial.println(time_output);
+    //Serial.println();
+    sekunden.einer = timeinfo.tm_sec % 10;
+    sekunden.zehner = (timeinfo.tm_sec / 10) % 10;
+    minuten.einer = timeinfo.tm_min % 10;
+    minuten.zehner = (timeinfo.tm_min / 10) % 10;
+    stunden.einer = timeinfo.tm_hour % 10;
+    stunden.zehner = (timeinfo.tm_hour / 10) % 10;
+#ifdef DEBUG
+    // Uhr Ausgabe:
+    Serial.print(stunden.zehner);
+    Serial.print(" ");
+    Serial.print(stunden.einer);
+    Serial.print(" - ");
+    Serial.print(minuten.zehner);
+    Serial.print(" ");
+    Serial.print(minuten.einer);
+    Serial.print(" - ");
+    Serial.print(sekunden.zehner);
+    Serial.print(" ");
+    Serial.print(sekunden.einer);
+    Serial.println(" ");
+#endif
+
+  }
+  return true;
+}
+
+
+
+
+
+void showTime(tm localTime) {
+  Serial.print(localTime.tm_mday);
+  Serial.print('-');
+  Serial.print(localTime.tm_mon + 1);
+  Serial.print('-');
+  Serial.print(localTime.tm_year - 100);
+  Serial.print('_');
+  Serial.print(localTime.tm_hour);
+  Serial.print(':');
+  Serial.print(localTime.tm_min);
+  Serial.print(':');
+  Serial.print(localTime.tm_sec);
+  Serial.print(" Day of Week ");
+  if (localTime.tm_wday == 0)   Serial.println(7);
+  else Serial.println(localTime.tm_wday);
+}
