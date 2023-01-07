fffa25219b75d17d1ad934f5279863e254a9254b
Thorsten Muelhoefer <muethos76@gmail.com>
2023-01-07 15:01:44 +0100

    auxillery hinzugefügt

---
+++ auxillery.ino
@@ -0,0 +1,14 @@
+void display_aux(){
+  if (fadein) {
+    fadeValue+=3;
+    if (fadeValue >= 200){fadein = 0;}
+  }else{
+    fadeValue-=6;
+    if (fadeValue <= 0){fadein = 1;}
+  }
+  PIXELS_AUX.setPixelColor(0, PIXELS_AUX.Color(warning.g, fadeValue, warning.b));
+
+  PIXELS_AUX.show();
+  
+}
+//test
